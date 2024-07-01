2882c394579d161d50732cde2dda0acbdadc2592
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-07-01 15:16:24 +0530

    coin calculator

--- src/components/currency-calculations/CurrencyCalculation.js
+++ src/components/currency-calculations/CurrencyCalculation.js
@@ -1,48 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CurrencyCalculation = () => {
+  const [amount1, setAmount1] = useState(1);
+  const [amount2, setAmount2] = useState(0);
+  const [currency1, setCurrency1] = useState("OBC");
+  const [currency2, setCurrency2] = useState("USD");
+  const [rates, setRates] = useState({});
+
+  useEffect(() => {
+    // Fetch exchange rates here
+    // For demonstration, we'll use a mock rate
+    setRates({
+      OBC: { USD: 8003.23, BTC: 0.87 },
+      BTC: { USD: 9205.68, OBC: 1.15 },
+      USD: { OBC: 0.000125, BTC: 0.000109 }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (rates[currency1] && rates[currency1][currency2]) {
+      setAmount2((amount1 * rates[currency1][currency2]).toFixed(2));
+    }
+  }, [amount1, currency1, currency2, rates]);
+
+  const handleAmount1Change = (e) => {
+    setAmount1(e.target.value);
+  };
+
+  const handleCurrency1Change = (e) => {
+    setCurrency1(e.target.value);
+  };
+
+  const handleCurrency2Change = (e) => {
+    setCurrency2(e.target.value);
+  };
+
   return (
     <div className="dg__calculation__area bg__color--1 poss--relative section-padding--xlg">
       <div className="container">
         <div className="row">
           <div className="col-lg-6">
             <div className="dg__calculation">
-              <h2>Calculation is a perfect way to get dessition</h2>
+              <h2>Calculation is a perfect way to get decision</h2>
               <p>
-                Lorem Ipsum is simply dummy the printing and typestting
-                industry. Lorem Ipsum has been the industry's stndard dummy
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
                 text. Lorem Ipsum is simply dummy text of the printing and
                 typesetting industry.
               </p>
               <div className="calculate__box">
-                <p>Currency Calculator.</p>
+                <p>Currency Calculator</p>
                 <input
                   className="btc-conterter"
-                  type="text"
-                  placeholder={1}
-                  data-current-rate="9205.68"
+                  type="number"
+                  value={amount1}
+                  onChange={handleAmount1Change}
                 />
                 <span>=</span>
                 <input
                   className="btc-coverter-output"
-                  type="text"
-                  placeholder="8003.23"
+                  type="number"
+                  value={amount2}
+                  readOnly
                 />
                 <div className="dg__select__option">
-                  <select>
-                    <option>Bitcoin (BTC)</option>
-                    <option>BUZZ (BUZZ)</option>
-                    <option>BVC (BVC)</option>
-                    <option>BXT (BXT)</option>
-                    <option>BXT (BXT)</option>
+                  <select value={currency1} onChange={handleCurrency1Change}>
+                    <option value="OBC">OneBond Coin (OBC)</option>
+                    <option value="BTC">Bitcoin (BTC)</option>
+                    <option value="USD">US Dollar (USD)</option>
                   </select>
-                  <select className="space--left">
-                    <option>US Dollar (USD)</option>
-                    <option>PPT (PPT)</option>
-                    <option>PRC (PRC)</option>
-                    <option>PRG (PRG)</option>
-                    <option>PRO (PRO)</option>
-                    <option>PTA (PTA)</option>
+                  <select 
+                    className="space--left" 
+                    value={currency2} 
+                    onChange={handleCurrency2Change}
+                  >
+                    <option value="USD">US Dollar (USD)</option>
+                    <option value="OBC">OneBond Coin (OBC)</option>
+                    <option value="BTC">Bitcoin (BTC)</option>
                   </select>
                 </div>
               </div>
