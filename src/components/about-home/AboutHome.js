9e84c059344a15e89b490b4a23003301fa692d9c
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-07-10 14:08:36 +0530

    about component read more navigation changed

--- src/components/about-home/AboutHome.js
+++ src/components/about-home/AboutHome.js
@@ -26,7 +26,7 @@ const AboutHome = () => {
               </p>
               <Link
                 className="secure__btn dg__btn btn--trans"
-                to={process.env.PUBLIC_URL + "/about"}
+                to={process.env.PUBLIC_URL + "/"}
               >
                 read more
               </Link>
