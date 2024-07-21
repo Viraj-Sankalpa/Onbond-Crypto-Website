ab2c8839972c8f57042cc60209a4813f6bd1c4d9
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-07-21 10:02:37 +0530

    header section remove from mobile screens

--- src/containers/headers/Header.js
+++ src/containers/headers/Header.js
@@ -1,31 +1,10 @@
 import PropTypes from "prop-types";
-import React, { useEffect, useState } from "react";
-import HeaderBtn from "../../components/header/HeaderBtn";
+import React from "react";
 import Logo from "../../components/header/Logo";
-import MobileMenu from "../../components/header/MobileMenu";
 
 const Header = ({ theme }) => {
-  const [scroll, setScroll] = useState(0);
-  const [headerTop, setHeaderTop] = useState(0);
-
-  useEffect(() => {
-    const header = document.querySelector("header");
-    setHeaderTop(header.offsetTop);
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const handleScroll = () => {
-    setScroll(window.scrollY);
-  };
   return (
-    <header
-      className={`dg__header header--absolute space-right-left ${
-        scroll > headerTop ? "stick" : ""
-      }`}
-    >
+    <header className="dg__header header--absolute space-right-left">
       <div className="container-fluid">
         <div className="row align-items-center">
           <div className="col-lg-2 col-xl-2 col-6">
@@ -42,18 +21,18 @@ const Header = ({ theme }) => {
           </div>
           <div className="col-lg-3 col-xl-2 col-6">
             {/* header buttons */}
-            <HeaderBtn />
+            {/* <HeaderBtn /> */}
           </div>
         </div>
       </div>
       {/* Mobile Menu */}
-      <MobileMenu />
+      {/* <MobileMenu /> */}
     </header>
   );
 };
 
 Header.propTypes = {
-  theme: PropTypes.string
+  theme: PropTypes.string,
 };
 
 export default Header;
--- src/containers/teams/TeamMembers.js
+++ src/containers/teams/TeamMembers.js
@@ -9,12 +9,12 @@ const members = [
     description:
       "Mr.Balaji has strategic oversight of the company’s broad investment portfolio and special projects at the group level. He had been a board member of various investment companies and various other companies in the past and is mandated to develop Wish Group investment’s policies, establish investment guidelines, and review proposed projects and investments to ensure they are in line with business objectives.",
     image: "images/team/md-img/balaji.png",
-    social: {
-      facebook: "#",
-      twitter: "#",
-      google: "#",
-      youtube: "#",
-    },
+    // social: {
+    //   facebook: "#",
+    //   twitter: "#",
+    //   google: "#",
+    //   youtube: "#",
+    // },
   },
   {
     name: "Mr. SHYAM SUNDAR",
@@ -22,12 +22,12 @@ const members = [
     description:
       "Mr. Shyam is a result-oriented finance professional with over 25 years of extensive experience in financial control, treasury management, acquisitions and exits. He provides strategic financial direction to the organization, aligned with the business strategy and actively participates in the organization’s decision-making process. He has a bottom-line driven approach that is focused on delivering results. ",
     image: "images/team/md-img/shyam.png",
-    social: {
-      facebook: "#",
-      twitter: "#",
-      google: "#",
-      youtube: "#",
-    },
+    // social: {
+    //   facebook: "#",
+    //   twitter: "#",
+    //   google: "#",
+    //   youtube: "#",
+    // },
   },
   {
     name: "Mr. DINESH KUMAR SABHARWAL",
@@ -35,12 +35,12 @@ const members = [
     description:
       "With 30 years of experience, Dinesh Kumar Sabharwal has established a reputation for delivering exceptional legal and financial advisory services to a diverse clientele, including multinational corporations, SMEs, and high-net-worth individuals. As both an advocate and a chartered accountant, Dinesh Kumar Sabharwal offers a unique blend of legal acumen and financial expertise, ensuring comprehensive and strategic solutions for complex business challenges. ",
     image: "images/team/md-img/dinesh.png",
-    social: {
-      facebook: "#",
-      twitter: "#",
-      google: "#",
-      youtube: "#",
-    },
+    // social: {
+    //   facebook: "#",
+    //   twitter: "#",
+    //   google: "#",
+    //   youtube: "#",
+    // },
   },
 ];
 
@@ -72,12 +72,12 @@ const TeamMembers = () => {
             <h2>{currentMember.name}</h2>
             <h3>{currentMember.position}</h3>
             <p>{currentMember.description}</p>
-            <div className="social-links">
+            {/* <div className="social-links">
               <a href={currentMember.social.facebook}>facebook</a>
               <a href={currentMember.social.twitter}>twitter</a>
               <a href={currentMember.social.google}>google</a>
               <a href={currentMember.social.youtube}>youtube</a>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
