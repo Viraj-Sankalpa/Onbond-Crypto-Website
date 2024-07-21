528d03f30cd073d5bbbace6058017f410c2ee1d2
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-07-21 10:10:28 +0530

    coin components responsive

--- src/components/coin-component/CoinComponent.js
+++ src/components/coin-component/CoinComponent.js
@@ -2,14 +2,10 @@ import React from "react";
 import "../../assets/scss/blocks/CoinComponent.scss";
 
 const CoinComponent = () => {
-
-
   return (
     <div className="coin-container">
-      
-
       <div className="buttons">
-      <a href="http://onebond.org/public" className="secure__btn dg__btn btn--trans">
+        <a href="http://onebond.org/public" className="secure__btn dg__btn btn--trans">
           Log In
         </a>
         <a href="http://onebond.org/public" className="secure__btn dg__btn btn--trans">
@@ -19,8 +15,6 @@ const CoinComponent = () => {
           Member login
         </a>
       </div>
-
-     
     </div>
   );
 };
